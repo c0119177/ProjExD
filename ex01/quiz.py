e7058622f495e5b53208430b543034aac4fd5702
c0119177 <c011917701@edu.teu.ac.jp>
2022-06-14 15:49:00 +0900

    サザエのつぼ焼きv1.01

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,19 +1,21 @@
+from dataclasses import dataclass
 import random
 import datetime
-def quiz(i):
+def quiz():
+    print("問題")
+    st = datetime.datetime.now()
+    i = random.randint(0, 2)
     data = [["サザエの旦那の名前は？: ", "カツオの妹の名前は？: ", "タラオはカツオから見てどんな関係？: "], 
     ["masuo", "ますお", "マスオ"], 
     ["wakame", "わかめ", "ワカメ"], 
     ["oi", "甥", "おい", "甥っ子", "おいっこ"]]
     a = input(data[0][i])
     if a.lower() in data[i + 1]:
-        return "正解"
+        print("正解")
     else:
-        return "出直してこい"
-
-if __name__ == "__main__":
-    print("問題")
-    st = datetime.datetime.now()
-    print(quiz(random.randint(0, 2)))
+        print("出直してこい")
     ed = datetime.datetime.now()
     print("所要時間:" + str((ed - st).seconds) + "秒")
+
+if __name__ == "__main__":
+    quiz()
