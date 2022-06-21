26ffa02f833e887713dd4ba7f998ac101bd95e79
c0119177 <c011917701@edu.teu.ac.jp>
2022-06-21 14:32:22 +0900

    v1.01

--- ex02/calc.py
+++ ex02/calc.py
@@ -14,18 +14,20 @@ class Calculater(object):
         self.calc_str = "0"
 
         calc.title("calculator")
-        calc.geometry("300x500")
+        calc.geometry("300x600")
 
         calc_frame = ttk.Frame(calc, width=300, height=50) # 計算式と結果用のFrame
         calc_frame.propagate(False) # サイズが固定される
-        calc_frame.pack(side=tk.TOP, padx=10, pady=20) # 余白の設定
+        calc_frame.pack(side=tk.TOP, pady=20) # 余白の設定
         button_frame = ttk.Frame(calc, width=300, height=400) # 計算ボタン用のFrame
         button_frame.propagate(False) # サイズが固定される
         button_frame.pack(side=tk.BOTTOM)
 
         self.calc_var = tk.StringVar()
-        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("Times New Roman",20))
-        calc_label.pack(anchor=tk.E)
+#        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("Times New Roman",20))
+#        calc_label.pack(anchor=tk.E)
+        calc_ent = ttk.Entry(calc_frame, textvariable = self.calc_var, font=("Times New Roman",40))
+        calc_ent.pack(anchor = tk.E)
 
         for y, row in enumerate(BUTTON, 1):
                 for x, num in enumerate(row):
@@ -33,16 +35,11 @@ class Calculater(object):
                     button.grid(row = y, column = x)
                     button.bind("<Button-1>", self.button_click)
 
-    #    for i in range(10):
-    #        button = tk.Button(calc, text = i, font = ("Times New Roman", 30), width = 4, height = 2)
-    #        button.grid(row = (9 - i) // 3, column = (9 - i) % 3)
-
-
     def button_click(self, event):
         click = event.widget["text"]
-#        self.calc_var += click
-#        self.calc_var.set(click)
-        tkm.showinfo(click, click)
+        self.calc_var += click
+        self.calc_var.set(click)
+#        tkm.showinfo(click, click)
 
 
 def main():
