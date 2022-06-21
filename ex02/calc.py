0c164cd914197bc234c9e189e5fe0f8af2c65246
c0119177 <c011917701@edu.teu.ac.jp>
2022-06-21 14:58:59 +0900

    v1.02 entry use

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.messagebox as tkm
 
+from numpy import insert
+
 BUTTON = [
     ["9", "8", "7"],
     ["6", "5", "4"],
@@ -10,37 +12,35 @@ BUTTON = [
     ]
 class Calculater(object):
     def __init__(self, calc = None):
-        
-        self.calc_str = "0"
-
         calc.title("calculator")
-        calc.geometry("300x600")
 
-        calc_frame = ttk.Frame(calc, width=300, height=50) # 計算式と結果用のFrame
-        calc_frame.propagate(False) # サイズが固定される
-        calc_frame.pack(side=tk.TOP, pady=20) # 余白の設定
-        button_frame = ttk.Frame(calc, width=300, height=400) # 計算ボタン用のFrame
-        button_frame.propagate(False) # サイズが固定される
+        calc_frame = ttk.Frame(calc, width=300, height=50)
+        calc_frame.propagate(False)
+        calc_frame.pack(side=tk.TOP, pady=20)
+        button_frame = ttk.Frame(calc, width=300, height=400)
+        button_frame.propagate(False)
         button_frame.pack(side=tk.BOTTOM)
 
-        self.calc_var = tk.StringVar()
-#        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("Times New Roman",20))
-#        calc_label.pack(anchor=tk.E)
-        calc_ent = ttk.Entry(calc_frame, textvariable = self.calc_var, font=("Times New Roman",40))
-        calc_ent.pack(anchor = tk.E)
+        self.calc_entry = tk.Entry(calc_frame, font=("Times New Roman",20))
+        self.calc_entry.pack(anchor=tk.E)
 
         for y, row in enumerate(BUTTON, 1):
                 for x, num in enumerate(row):
                     button = tk.Button(button_frame, text = num, font = ("Times New Roman", 30), width = 4, height = 2)
                     button.grid(row = y, column = x)
                     button.bind("<Button-1>", self.button_click)
+                    button.bind("<Enter>", self.enter_bg)
+                    button.bind("<Leave>", self.leave_bg)
 
     def button_click(self, event):
         click = event.widget["text"]
-        self.calc_var += click
-        self.calc_var.set(click)
+        self.calc_entry.insert(tk.END, click)
 #        tkm.showinfo(click, click)
 
+    def enter_bg(self,event):
+        enter = event.widget["bg"] = "#F5F5F5"
+    def leave_bg(self,event):
+        leave = event.widget["bg"] = "SystemButtonFace"
 
 def main():
     calc = tk.Tk()
