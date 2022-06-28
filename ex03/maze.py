9c4184208f77252584b5eabb5c61de900544fad5
c0119177 <c011917701@edu.teu.ac.jp>
2022-06-28 16:59:08 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import maze_maker as mm
+from PIL import Image, ImageTk
+import tkinter.messagebox as tkm
+
+roop = None
 
 def key_Down(event):
     global key
@@ -10,29 +14,38 @@ def key_Up(event):
     key = ""
 
 def main_proc():
-    global cx, cy, mx, my
+    global cx, cy, mx, my, roop
     if key == "Up" and maze_bg[my-1][mx] == 0: my -= 1
     if key == "Down" and maze_bg[my+1][mx] == 0: my += 1
     if key == "Left" and maze_bg[my][mx-1] == 0: mx -= 1
     if key == "Right" and maze_bg[my][mx+1] == 0: mx += 1
-    cx, cy = mx * 100 + 50, my * 100 + 50
+    cx, cy = mx * 50 + 25, my * 50 + 25
     canvas.coords("tori1", cx, cy)
-    maze.after(75, main_proc)
+    if cx == 1375 and cy ==775:
+        maze.after_cancel(roop)
+        roop = None
+        tkm.showinfo("ゴールしました", "Congratulations‼")
+        return
+    roop = maze.after(75, main_proc)
 
 if __name__ == "__main__":
     maze = tk.Tk()
     maze.title("迷えるこうかとん")
 
-    canvas = tk.Canvas(maze, width = 1500, height = 900, bg = "black")
+    canvas = tk.Canvas(maze, width = 1450, height = 900, bg = "black")
     canvas.pack()
 
-    maze_bg = mm.make_maze(15, 9)
+    maze_bg = mm.make_maze(29, 17)
     mm.show_maze(canvas, maze_bg)
 
-    tori1 = tk.PhotoImage(file = "../fig/9.png")
+    canvas.create_rectangle(50, 50, 100, 100, fill="orange")
+    canvas.create_rectangle(1350, 750, 1400, 800, fill="blue")
+
+    tori = Image.open("../fig/0.png").resize((25, 25))
+    tori = ImageTk.PhotoImage(tori)
     mx, my = 1, 1
-    cx, cy = mx * 100 + 50, my * 100 + 50
-    canvas.create_image(cx, cy, image = tori1, tag = "tori1")
+    cx, cy = mx * 50 + 25, my * 50 + 25
+    canvas.create_image(cx, cy, image = tori, tag = "tori1")
 
     key = ""
     maze.bind("<KeyPress>", key_Down)
