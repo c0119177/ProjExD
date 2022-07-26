3225b5a1c589ed72a62b79af943de460f641fcbb
c0119177 <c011917701@edu.teu.ac.jp>
2022-07-26 13:41:45 +0900

    ver2

--- ex06/jump_kokaton.py
+++ ex06/jump_kokaton.py
@@ -1,7 +1,8 @@
 import pygame as pg
 import sys
 import random
-import numpy as np
+import time
+import math
 
 #スクリーン用クラス
 class Screen:
@@ -12,11 +13,20 @@ class Screen:
         self.bg_sfc = pg.image.load(img).convert()
         self.bg_x = 0
 
+    def game_over_screen(self):
+        font = pg.font.Font(None, 120)
+        txt = font.render("GameOver", True, (0, 0, 0))
+        self.sfc.blit(txt, (600, 400))
+        pg.display.update()
+
     def blit(self):
         #背景を動かして描画する
         self.sfc.blit(self.bg_sfc, [self.bg_x - self.rect.width, 0])
         self.sfc.blit(self.bg_sfc, [self.bg_x, 0])
         self.bg_x = (self.bg_x - 5) % self.rect.width
+
+    def text(self,text):
+        self.sfc.blit(text,[10,5])
 
 #こうかとん用クラス
 class Bird:
@@ -46,12 +56,12 @@ class Bird:
 
 #障害物用クラス
 class Obstacle:
-    def __init__(self, img, size: float, speed, screen: Screen):
+    def __init__(self, img, size: float, speed, height: tuple, screen: Screen):
         self.sfc = pg.image.load(img)
         self.sfc = pg.transform.rotozoom(self.sfc, 0, size)
         self.rect = self.sfc.get_rect()
         self.rect.x = screen.rect.width + self.rect.width
-        self.rect.y = screen.rect.height - self.rect.height
+        self.rect.y = screen.rect.height - self.rect.height - random.randint(height[0], height[1])
         self.vx = -1 * speed
         self.vy = 0
 
@@ -65,11 +75,16 @@ class Obstacle:
 
 def main():
     #fpsのカウント開始
+    pg.init()
     clock = pg.time.Clock()
     sc = Screen("飛べ！こうかとん", (1600, 900), "../fig/bg_sabaku.jpg")
     tori = Bird("../fig/2.png", 2.0, sc)
-    obs = Obstacle("../fig/sabo_ver02.png", 4.0, 10, sc)
-    obs2 = Obstacle("../fig/sabo_ver02.png", 2.0, 12, sc)
+    obs = Obstacle("../fig/sabo_ver03.png", 4.0, 10, (0, 0), sc)
+    obs2 = Obstacle("../fig/sabo_ver03.png", 2.0, 12, (0, 0), sc)
+    obs_tori = Obstacle("../fig/tori_ver01.png", 2.0, 12, (0, 10), sc)
+    start = time.time()
+    font = pg.font.Font(None,100)
+    dif = 0
 
     #描画
     while True:
@@ -78,27 +93,57 @@ def main():
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                return
+                pg.quit()
+                sys.exit()
 
         tori.update(sc)
         obs.update(sc)
-        obs2.update(sc)
+        if dif >= 5:
+            obs2.update(sc)
+        if dif >= 10:
+            obs_tori.update
 
-        if tori.rect.colliderect(obs.rect):
-            return
-        if tori.rect.colliderect(obs2.rect):
-            return
+        collision(tori.rect, obs.rect, sc)
+        collision(tori.rect, obs2.rect, sc)
+        collision(tori.rect, obs_tori.rect, sc)
 
-        #障害物が画面外に出たときに再配置する
-        if obs.rect.x <  -1 * random.randint(obs.rect.width, obs.rect.width * 5):
-            obs.rect.x = sc.rect.width + obs.rect.width
-        if obs2.rect.x <  -1 * random.randint(obs2.rect.width, obs2.rect.width * 5 ):
-            obs2.rect.x = sc.rect.width + obs2.rect.width
+        respawn(obs, (0, 0), sc)
+        respawn(obs2, (0, 0), sc)
+        respawn(obs_tori, (0, 10), sc)
+
+        end = end = time.time()
+        dif = end - start 
+        dif = math.floor(dif)
+        text = font.render(str(dif),True,(64,255,63))
+        sc.text(text)
+
         pg.display.update()
         clock.tick(120)
 
-if __name__ == "__main__":
-    pg.init()
-    main()
+def collision(tori, obs, screen: Screen):
+    if tori.colliderect(obs):
+        screen.game_over_screen()
+        Quit()
+            
+def Quit():
+    END_flg=True
+    while END_flg==True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                END_flg = False
+            if pg.key.get_pressed()[pg.K_ESCAPE]:
+                END_flg = False
+            elif event.type == pg.KEYDOWN:
+                END_flg = False
+                main()
     pg.quit()
     sys.exit()
+
+#障害物が画面外に出たときに再配置する
+def respawn(obs: Obstacle, height: tuple, screen: Screen):
+    if obs.rect.x <  -1 * random.randint(obs.rect.width, obs.rect.width * 5):
+            obs.rect.x = screen.rect.width + obs.rect.width
+            obs.rect.y = screen.rect.height - obs.rect.height - random.randint(height[0], height[1])
+
+if __name__ == "__main__":
+    main()
